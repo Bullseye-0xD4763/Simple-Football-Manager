8d424451d79be49d4f638bf3a3b16f4564ade3f0
Luis <luisgmsferreira@gmail.com>
2021-04-30 18:28:46 +0100

    .

--- src/Forwards.java
+++ src/Forwards.java
@@ -1,27 +1,29 @@
-public class Forwards extends Player{
+public class Forwards extends Player {
     public int finalization;
     public int composture;
     public int drible;
-    
-    public Forwards(){
-      public Forwards(String PlrName, int PlrNumber, int Atc, int Def, int Drb, int finalization, int composture, int drible){
-        super(String PlrName, int PlrNumber, int Atc, int Def, int Drb);
-        this.finalization=finalization;
-        this.composture=composture;
-        this.drible=drible;
+
+    public Forwards() {
+      public Forwards(String PlrName, int PlrNumber, int Atc, int Def, int Drb, int finalization, int composture,
+        int drible){
+            super(String PlrName, int PlrNumber, int Atc, int Def, int Drb);
+            this.finalization = finalization;
+            this.composture = composture;
+            this.drible = drible;
+        }
+
+        public int getFinalization () {
+            return this.finalization;
+        }
+
+        public int getComposture () {
+            return this.composture;
+        }
+
+        public int getDrible () {
+            return this.drible;
+        }
+
+
     }
-    
-    public int getFinalization(){
-        return this.finalization;
-    }
-    
-    public int getComposture(){
-        return this.composture;
-    }
-    
-    public int getDrible(){
-        return this.drible;
-    }
-    
-    
 }
