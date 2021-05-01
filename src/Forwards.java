3e13048916184da17cde3304daae4534987caae9
Luís Ferreira <luisgmsferreira@gmail.com>
2021-05-01 20:05:29 +0100

    Update Forwards.java

--- src/Forwards.java
+++ src/Forwards.java
@@ -1,62 +1,112 @@
 public class Forwards extends Players{
-    public int finalization;
-    public int dribble;
-    public int posture;
+    public int center;
+    public int speed;
+    public int endurance;
+    public int dexterity;
+    public int propulsion;
+    public int airgame;
+    public int kick;
     public int pass;
+    public int precision;
 
 
     //construtor de criar as variaveis vazias
     public Forwards(){
         super();
-        this.finalization = 0;
-        this.dribble = 0;
-        this.posture = 0;
+        this.center = 0;
+        this.speed = 0;
+        this.endurance = 0;
+        this.dexterity = 0;
+        this.propulsion = 0;
+        this.airgame = 0;
+        this.kick = 0;
         this.pass = 0;
+        this.precision = 0;
     }
 
     //Pega nos argumentos
-    public Forwards(String PlrName, int PlrNumber, int Atc, int Def, int Drb, int finalization, int dribble, int posture){
-        super(PlrName, PlrNumber, Atc, Def, Drb, finalization, dribble, posture);
-        this.finalization = finalization;
-        this.dribble = dribble;
-        this.posture = posture;
+    public Forwards(String PlrName, int PlrNumber, int center, int Atc, int Def, int Drb, int speed, int endurance,
+                       int dexterity, int propulsion, int airgame, int kick, int pass, int precision){
+        super(PlrName, PlrNumber, Atc, Def, Drb, speed, endurance, dexterity);
+        this.center = center;
+        this.speed = speed;
+        this.endurance = endurance;
+        this.dexterity = dexterity;
+        this.propulsion = propulsion;
+        this.airgame = airgame;
+        this.kick = kick;
         this.pass = pass;
+        this.precision = precision;
     }
 
     //criar duas instancias de jogador || Pedro.Forwards(Ze)
     public Forwards(Forwards placeholder){
         super(placeholder);
-        this.finalization = placeholder.finalization;
-        this.dribble = placeholder.dribble;
-        this.posture = placeholder.posture;
+        this.center = placeholder.center;
+        this.speed = placeholder.speed;
+        this.endurance = placeholder.endurance;
+        this.dexterity = placeholder.dexterity;
+        this.propulsion = placeholder.propulsion;
+        this.airgame = placeholder.airgame;
+        this.kick = placeholder.kick;
         this.pass = placeholder.pass;
+        this.precision = placeholder.precision;
     }
 
     //getters e setters
-    public int getPosture(){
-        return this.posture;
+    public int getCenter(){
+        return this.center;
     }
-    public int getDribble(){
-        return this.dribble;
+    public int getSpeed(){
+        return this.speed;
     }
-    public int getFinalization(){
-        return this.finalization;
+    public int getEndurance(){
+        return this.endurance;
+    }
+    public int getDexterity(){
+        return this.dexterity;
+    }
+    public int getPropulsion(){
+        return this.propulsion;
+    }
+    public int getAirGame(){
+        return this.airgame;
+    }
+    public int getKick(){
+        return this.kick;
     }
     public int getPass(){
         return this.pass;
     }
+    public int getPrecision(){
+        return this.precision;
+    }
 
-
-    public void setPosture(){
-        this.posture = posture;
+    public int setCenter(){
+        return this.center;
     }
-    public void setDribble(){
-        this.dribble = dribble;
+    public int setSpeed(){
+        return this.speed;
     }
-    public void setFinalization(){
-        this.finalization = finalization;
+    public int setEndurance(){
+        return this.endurance;
     }
-    public void setPass() {
-        this.pass = pass;
+    public int setDexterity(){
+        return this.dexterity;
+    }
+    public int setPropulsion(){
+        return this.propulsion;
+    }
+    public int setAirGame(){
+        return this.airgame;
+    }
+    public int setKick(){
+        return this.kick;
+    }
+    public int setPass(){
+        return this.pass;
+    }
+    public int setPrecision(){
+        return this.precision;
     }
 }
