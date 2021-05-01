6005df7e78b2de75d7de83bdbb55150a8cad0098
JBB-do-mal <82215539+JBB-do-mal@users.noreply.github.com>
2021-05-01 20:31:24 +0100

    Update Stats.java

--- src/Stats.java
+++ src/Stats.java
@@ -1,3 +1,37 @@
 public class Stats extends Players{
-    //this one still needs
+    
+
+
+
+    //Stats dos players
+    public int speed;
+    public int endurance;
+    public int dexterity;
+    public int propulsion;
+    public int airgame;
+    public int kick;
+    public int pass;
+
+    //Stats extra
+    public int flexibility; //goalkeepres
+    public int tackle; //defenders
+    public int getball; //midfielders
+    public int longpass; //laterais
+
+
+    //Stats de jogo
+    public int score_team1;
+    public int score_team2;
+    
+    
+    public double Attack(ArrayList(jogadores)){}
+    public double Defense(ArrayList jogadores){}
+
+    
+    
+    
+    
+    
+    
+    
 }
