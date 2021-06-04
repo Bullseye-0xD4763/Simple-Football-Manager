1380ee47ede46dadb673daad2d7baab66034c81c
Luis <luisgmsferreira@gmail.com>
2021-06-04 20:00:10 +0100

    Tentei
    .-.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,44 @@
-
-
-
-
 package proj;
-import java.util.*;
+
+import java.util;
 import java.util.stream.Collectors;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
-        Parser.parse();
+class PrintAndScan{
+    //criar variavel vazia
+    private String option;
+    public PrintAndScan(){
+        this.option = ' ';
+    }
+    //recebe argumento e copia o seu valor para a 'option'
+    public PrintAndScan(String option){
+        this.option = option;
+    }
+    //copia PrintAndScan para outro PrintAndScan
+    public PrintAndScan(PrintAndScan var) {
+        this.option = var.getOption();
     }
 
-}
-
-class PrintAndScan {
-    public static void printmenu() {
-
-        System.out.print("Enter one of the following options: " + "\n1. Start game" + "\n2. Leave Game"
-                + "\n3. Load Save" + "\n4. Manage Team" + "\n5. Create Team" + "\n6. Create Player" + "\n7. Credits\n");
+    // getter
+    public String getOption() {
+        return this.option;
     }
 
-    public static void main(String args[]) {
+    // setter
+    public void setOption(String var2) {
+        this.option = var2;
+    }
+    
+
+
+
+
+    public static void menuPrint(String option) {
+        System.out.println("Enter one of the following options: "+"\n1. Start game"+"\n2. Leave Game"+"\n3. Load Save"+"\n4. Manage Team"+"\n5. Create Team"+"\n6. Create Player"+"\n7. Credits\n");
         boolean startLoop = false;
         while (startLoop == false) {
-            Scanner in = new Scanner(System.in);
-            printmenu();
             // string name é a capturada
-            char name = in.next().charAt(0);
-            switch (name) {
+            //char name = in.next().charAt(0);
+            switch (option) {
                 case '1':
                     System.out.println("Start");
                     // Start game
@@ -50,4 +59,17 @@ class PrintAndScan {
             }
         }
     }
+}
+
+public class Main {
+    
+    PrintAndScan printfunction = new PrintAndScan();
+
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+
+        Parser.parse();
+    }
+        String stdName = input.nextLine();
+        printfunction.menuPrint(stdName);
 }
