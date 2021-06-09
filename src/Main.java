c4f5e9b5ba110eb062ecc9ed634709abb0601626
JBB-do-mal <82215539+JBB-do-mal@users.noreply.github.com>
2021-06-09 18:39:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,49 @@
 package proj;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.SQLOutput;
 import java.util.*;
 
 
-public class Main{
+public class Main {
 
     public static boolean keepAlive;
     private static Scanner in;
 
 
-
-
-    public static void main(String[] args) throws LinhaIncorretaException {
+    public static void main(String[] args) throws LinhaIncorretaException, IOException {
         //Iniciar objetos
-        Parser p  = new Parser();
+        Parser p = new Parser();
         in = new Scanner(System.in);
 
 
         keepAlive = true;
         while (keepAlive == true) {
-            System.out.println("\nEnter one of the following options: "+"\n1. Start game"+"\n2. Load Save"+"\n3. Manage Team"+"\n4. Create Team"+"\n5. Create Player"+"\n6. Credits\n7. Exit\n");
+            System.out.println("\nEnter one of the following options: " + "\n1. Start game" + "\n2. Load Save" + "\n3. Manage Team" + "\n4. Create Team" + "\n5. Create Player" + "\n6. Credits\n7. Exit\n");
+            // string name é a capturada
             String stdName = in.nextLine();
-            switch (stdName) { 
+            switch (stdName) {
                 case "1":
                     System.out.println("Start");
                     Parser.parse();
-                    keepAlive = false;
+                    //keepAlive = false;
                     break;
                 case "2":
+
                     // load save
                     keepAlive = false;
                     break;
                 case "3":
-                    // Manage Team
+                    associatePlayer();
                     keepAlive = false;
                     break;
                 case "4":
-                    // Create Team
+                    createTeam();
                     keepAlive = false;
                     break;
                 case "5":
@@ -66,34 +67,100 @@ public class Main{
             }
         }
     }
+    //3. Manage team
+    private static void associatePlayer() {
+        //print jogadores em equipa TEMP
+        /*System.out.println("Nome do jogador a transferir");String nome = in.nextLine();
+        System.out.println("Nome de equipa de destino"); String destino = in.nextLine();*/
+
+        String a = "Equipa: Temp", b = "Equipa: Temp\nAvancado:Jose-56,23,123,123,23,";
 
 
-
-    public static Jogador createPlayer(){
-        System.out.println("Nome do jogador:"); String name = in.nextLine();
-        System.out.println("Nº Jogador: "); int number = in.nextInt();
-        System.out.println("Velocidade: "); int velocidade = in.nextInt();
-        System.out.println("Resistência: "); int resistencia = in.nextInt();
-        System.out.println("Destreza: "); int destreza = in.nextInt();
-        System.out.println("Impulsao: "); int impulsao = in.nextInt();
-        System.out.println("Cabeça: "); int cabeca = in.nextInt();
-        System.out.println("Remate: "); int remate = in.nextInt();
-        System.out.println("Passe: ");  int passe = in.nextInt();
-
-        Jogador j = new Jogador(name,number,velocidade,resistencia,destreza,impulsao,cabeca,remate,passe);
-        return j;
+        //Print equipas
     }
-    
-    private static void createTeam(){
-        System.out.println("Nome da equipa:"); String name = in.nextLine();
+
+
+    //4. Create team
+    private static void createTeam() {
+        System.out.println("Nome da equipa:");
+        String name = in.nextLine();
         try {
             String content = "\nEquipa: " + name;
             Files.write(Paths.get("src/proj/Jogadores.txt"), content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-        } catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-}
 
+    //5. Create Player
+    private static Jogador createPlayer() throws IOException {
+        //Recebe estatísticas
+        System.out.println("Nome do jogador:");
+        String name = in.nextLine();
+        System.out.println("Nº Jogador: ");
+        int number = in.nextInt();
+        System.out.println("Velocidade: ");
+        int velocidade = in.nextInt();
+        System.out.println("Resistência: ");
+        int resistencia = in.nextInt();
+        System.out.println("Destreza: ");
+        int destreza = in.nextInt();
+        System.out.println("Impulsao: ");
+        int impulsao = in.nextInt();
+        System.out.println("Cabeça: ");
+        int cabeca = in.nextInt();
+        System.out.println("Remate: ");
+        int remate = in.nextInt();
+        System.out.println("Passe: ");
+        int passe = in.nextInt();
+
+        //Recebe posição
+        System.out.println("Posição: \n1. Avançado\n2. Defesa\n3. Medio\n4.Lateraln\n5. Guarda-Redes \n");
+        int posicao = in.nextInt();
+
+        //Procurar a linha onde está a equipa temp
+        List<String> lines;
+        Path mypath = Paths.get("src/proj/Jogadores.txt");
+        try {
+            lines = Files.readAllLines(mypath, StandardCharsets.UTF_8);
+        } catch (IOException exc) {
+            lines = new ArrayList<>();
+        }
+
+        for (String linha : lines){
+            if (linha == "Equipa: Temp"); System.out.println("DEU");
+
+
+        }
+
+
+            //Dependendo da posição escrever "Posição: Nome,vel,res,destr, etc..
+            switch (posicao) {
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Recuperação: ");
+                    int rec = in.nextInt();
+
+                    break;
+                case 4:
+                    System.out.println("Cruzamento: ");
+                    int cruz = in.nextInt();
+                    break;
+                case 5:
+                    System.out.println("Elasticidade");
+                    break;
+            }
+
+        Jogador j = new Jogador(name, number, velocidade, resistencia, destreza, impulsao, cabeca, remate, passe);
+
+        return j;
+    }
+
+
+}
 
 
