852ef1ee6e1710c92fc4c73542a9af7c1ced1ae8
JBB-do-mal <82215539+JBB-do-mal@users.noreply.github.com>
2021-06-10 14:42:07 +0100

    JÁ FAZ TRANSFERÊNCIAS

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static boolean keepAlive;
     private static Scanner in;
 
-
     public static void main(String[] args) throws LinhaIncorretaException, IOException {
         //Iniciar objetos
         Parser p = new Parser();
@@ -68,24 +67,30 @@ public class Main {
         }
     }
     //3. Manage team
-    private static void associatePlayer() {
+    private static void associatePlayer() throws IOException {
         //print jogadores em equipa TEMP
-        /*System.out.println("Nome do jogador a transferir");String nome = in.nextLine();
-        System.out.println("Nome de equipa de destino"); String destino = in.nextLine();*/
-
-        String a = "Equipa: Temp", b = "Equipa: Temp\nAvancado:Jose-56,23,123,123,23,";
 
 
-        //Print equipas
+        System.out.println("Nome do jogador a transferir ?");
+        String nome = in.nextLine();
+        System.out.println("Equipa");
+        String destino = in.nextLine();
+
+        //Se o jogador não tiver equipa
+        //updateDatabase("Equipa:"+destino, "Equipa:"+destino+"\n"+nome);
+
+        //Se o jogador tiver equipa
+        updateDatabase(nome,"");
+        updateDatabase("Equipa:"+destino, "Equipa:"+destino+"\n"+nome);
+
+       // updateDatabase();
     }
-
-
     //4. Create team
     private static void createTeam() {
         System.out.println("Nome da equipa:");
         String name = in.nextLine();
         try {
-            String content = "\nEquipa: " + name;
+            String content = "\nEquipa:" + name;
             Files.write(Paths.get("src/proj/Jogadores.txt"), content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -97,70 +102,65 @@ public class Main {
         //Recebe estatísticas
         System.out.println("Nome do jogador:");
         String name = in.nextLine();
-        System.out.println("Nº Jogador: ");
-        int number = in.nextInt();
-        System.out.println("Velocidade: ");
-        int velocidade = in.nextInt();
-        System.out.println("Resistência: ");
-        int resistencia = in.nextInt();
-        System.out.println("Destreza: ");
-        int destreza = in.nextInt();
-        System.out.println("Impulsao: ");
-        int impulsao = in.nextInt();
-        System.out.println("Cabeça: ");
-        int cabeca = in.nextInt();
-        System.out.println("Remate: ");
-        int remate = in.nextInt();
-        System.out.println("Passe: ");
-        int passe = in.nextInt();
+        System.out.println("Nº Jogador: ");int number = in.nextInt();
+        System.out.println("Velocidade: ");int velocidade = in.nextInt();
+        System.out.println("Resistência: ");int resistencia = in.nextInt();
+        System.out.println("Destreza: ");int destreza = in.nextInt();
+        System.out.println("Impulsao: ");int impulsao = in.nextInt();
+        System.out.println("Cabeça: ");int cabeca = in.nextInt();
+        System.out.println("Remate: ");int remate = in.nextInt();
+        System.out.println("Passe: ");int passe = in.nextInt();
 
         //Recebe posição
         System.out.println("Posição: \n1. Avançado\n2. Defesa\n3. Medio\n4.Lateraln\n5. Guarda-Redes \n");
         int posicao = in.nextInt();
 
-        //Procurar a linha onde está a equipa temp
-        List<String> lines;
-        Path mypath = Paths.get("src/proj/Jogadores.txt");
-        try {
-            lines = Files.readAllLines(mypath, StandardCharsets.UTF_8);
-        } catch (IOException exc) {
-            lines = new ArrayList<>();
-        }
-
-        for (String linha : lines){
-            if (linha == "Equipa: Temp"); System.out.println("DEU");
-
-
-        }
-
 
             //Dependendo da posição escrever "Posição: Nome,vel,res,destr, etc..
-            switch (posicao) {
-                case 1:
-
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    System.out.println("Recuperação: ");
-                    int rec = in.nextInt();
-
-                    break;
-                case 4:
-                    System.out.println("Cruzamento: ");
-                    int cruz = in.nextInt();
-                    break;
-                case 5:
-                    System.out.println("Elasticidade");
-                    break;
-            }
+        switch (posicao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                System.out.println("Recuperação: ");
+                int rec = in.nextInt();
+                break;
+            case 4:
+                System.out.println("Cruzamento: ");
+                int cruz = in.nextInt();
+                break;
+            case 5:
+                System.out.println("Elasticidade");
+                break;
+        }
 
         Jogador j = new Jogador(name, number, velocidade, resistencia, destreza, impulsao, cabeca, remate, passe);
+
+        String jo;
+        updateDatabase("Equipa:Temp","Equipa:Temp\n");
 
         return j;
     }
 
+    //Se não existir equipa não realiza alterações mas não retorna erro
+    public static void updateDatabase(String regex, String replacement) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        Scanner fileinput = new Scanner(new File("src/proj/Jogadores.txt"));
+        //Ler linhas e adicionar ao StringBuffer
+        while (fileinput.hasNextLine()){
+            buffer.append(fileinput.nextLine() + System.lineSeparator());
+        }
+        String fileContent = buffer.toString();
+        fileinput.close();
+
+        fileContent = fileContent.replaceAll(regex,replacement);
+
+        FileWriter writer = new FileWriter("src/proj/Jogadores.txt");
+        writer.append(fileContent);
+        writer.flush();
+    }
+
 
 }
-
 
