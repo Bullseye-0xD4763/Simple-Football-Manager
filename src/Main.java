b8468403e9103e880e09557ffa376e37972f350a
JBB-do-mal <82215539+JBB-do-mal@users.noreply.github.com>
2021-06-13 01:11:13 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,197 +1,226 @@
 package proj;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Parser {
+
+    public static Jogo startGame() throws LinhaIncorretaException{
+        Scanner in = new Scanner(System.in);
+        List<String> linhas = lerFicheiro("src/proj/Jogadores.txt");
+        Map<String, Equipa> equipasTotal = new HashMap<>();
+        Map<Integer, Jogador> jogadoresTotal = new HashMap<>();
+       // List<Jogo> jogos = new ArrayList<>();
+        Equipa ultima = null; Jogador j = null;
+        String[] linhaPartida;
 
 
-public class Main {
-
-    public static boolean keepAlive;
-    private static Scanner in;
-
-    public static void main(String[] args) throws LinhaIncorretaException, IOException {
-        //Iniciar objetos
-        Parser p = new Parser();
-        in = new Scanner(System.in);
-
-
-        keepAlive = true;
-        while (keepAlive == true) {
-            System.out.println("\nEnter one of the following options: " + "\n1. Start game" + "\n2. Display Saved Games" + "\n3. Manage Team" + "\n4. Create Team" + "\n5. Create Player" + "\n6. Credits\n7. Exit\n");
-            // string name é a capturada
-            String stdName = in.nextLine();
-            switch (stdName) {
-                case "1":
-                    System.out.println("Start");
-                    //Parser.parse();
-                    Jogo j = Parser.startGame();
-                    save(j);
-                    //keepAlive = false;
-
+        for (String linha : linhas) {
+            linhaPartida = linha.split(":", 2);
+            switch(linhaPartida[0]){
+                case "Equipa":
+                    Equipa e = Equipa.parse(linhaPartida[1]);
+                    equipasTotal.put(e.getNome(), e);
+                    ultima = e;
                     break;
-                case "2":
-                    // load save
-                    System.out.println("Saved Games: ");
-                    Parser.loadSave();
+                case "Guarda-Redes":
+                    j = GuardaRedes.parse(linhaPartida[1]);
+                    jogadoresTotal.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
                     break;
-                case "3":
-                    associatePlayer();
+                case "Defesa":
+                    j = Defesa.parse(linhaPartida[1]);
+                    jogadoresTotal.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
                     break;
-                case "4":
-                    createTeam();
-                    keepAlive = false;
+                case "Medio":
+                    j = Medio.parse(linhaPartida[1]);
+                    jogadoresTotal.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
                     break;
-                case "5":
-                    createPlayer();
+                case "Lateral":
+                    j = Lateral.parse(linhaPartida[1]);
+                    jogadoresTotal.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
                     break;
-                case "6":
-                    // Créditos
-                    System.out.println("Feito por: Cristiano Ronaldo");
-                    keepAlive = false;
-                    break;
-                case "7":
-                    // Leave Game
-                    keepAlive = false;
+                case "Avancado":
+                    j = Avancado.parse(linhaPartida[1]);
+                    jogadoresTotal.put(j.getNumeroJogador(), j); //.put coloca na
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
                     break;
                 default:
-                    // it will end the program
-                    System.out.println("Invalid option");
-                    keepAlive = false;
-                    in.close();
+                    break;
+            }
+        }
+
+
+        //Escolher Equipas
+
+        Equipa casa = null, fora = null;
+        System.out.println("Equipa a escolher:"); String equipaCasa = in.nextLine();
+        System.out.println("Equipa adversária: "); String equipaFora = in.nextLine();
+
+
+
+        for (Equipa e: equipasTotal.values()){
+            if (e.getNome().equals(equipaCasa)){ casa = e;}
+            if (e.getNome().equals(equipaFora)){ fora = e;}
+           // System.out.println(e.getNome());
+        }
+
+        //equipas nao podem ser null nem iguais
+        if (casa != null && fora != null && casa != fora){
+        System.out.println("\nEquipa escolhida------------\n ");System.out.println(casa.toString());
+        System.out.println("\nEquipa adversária-----------\n"); System.out.println(fora.toString());
+        }
+        else if(casa == null){ System.out.println("Equipa que escolheste não existe."); }
+        else if(fora == null){System.out.println("Equipa do oponente não existe."); }
+
+
+
+        //Escolher Jogadores - definir equipas
+        System.out.println("\n*\nMenu de escolha de Plantel------------------\nInsere o nº do jogador para o selecionar\n");
+        Equipa casaTitulares = Equipa.defineTeam(casa);
+        Equipa foraTitulares = Equipa.defineTeam(fora);
+        Equipa casaSuplentes = Equipa.getSuplentes(casa, casaTitulares);
+        Equipa foraSuplentes = Equipa.getSuplentes(fora, foraTitulares);
+
+        System.out.println("Main team: "+casaTitulares.toStringWithStats());
+        System.out.println("Secondary team: "+casaSuplentes.toStringWithStats());
+
+
+        //Definir estratégia
+        int stratCasa = 0, stratFora = 0;
+        while (!Equipa.legalStrat(stratCasa) && !Equipa.legalStrat(stratFora)) {
+            System.out.println("Tua Estratégia : (Insere no formato Int: 442)\n(4-4-2) (4-3-3) (5-4-1) (5-3-2)\n");stratCasa = in.nextInt();
+            if ((!Equipa.legalStrat(stratCasa))){ System.out.println("Estratégia ilegal."); }
+            System.out.println("Estratégia do adversário: (Insere no formato Int: 442)\n(4-4-2) (4-3-3) (5-4-1) (5-3-2)\n");stratFora = in.nextInt();
+            if ((!Equipa.legalStrat(stratFora))){ System.out.println("Estratégia ilegal."); }
+        }
+
+
+
+        //Jogar
+        Jogo jog =Jogo.Play(casaTitulares, foraTitulares, stratCasa , stratFora);
+        /**/
+        return jog;
+    }
+
+
+    public static void parse() throws LinhaIncorretaException {
+        List<String> linhas = lerFicheiro("src/proj/Jogadores.txt"); //Lista de Strings = informação ficheiro
+        Map<String, Equipa> equipas = new HashMap<>(); //Novo hashmap <CLube do ze, Equipa>
+        Map<Integer, Jogador> jogadores = new HashMap<>(); //Novo hashmap <23, Joao>
+        List<Jogo> jogos = new ArrayList<>();           //Lista do tipo jogo, com o nome "jogos"
+        Equipa ultima = null; Jogador j = null;         //Equipa ultima = null
+        String[] linhaPartida;                          //Array de strings
+        for (String linha : linhas) {
+            linhaPartida = linha.split(":", 2);
+            switch(linhaPartida[0]){
+                case "Equipa":
+                    System.out.println("\nTESTE\n linhaparatida[1] -> " + linhaPartida[1]);
+                    Equipa e = Equipa.parse(linhaPartida[1]);
+                    equipas.put(e.getNome(), e);
+                    ultima = e;
+                    break;
+                case "Guarda-Redes":
+                    j = GuardaRedes.parse(linhaPartida[1]);
+                    jogadores.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
+                    break;
+                case "Defesa":
+                    j = Defesa.parse(linhaPartida[1]);
+                    jogadores.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
+                    break;
+                case "Medio":
+                    j = Medio.parse(linhaPartida[1]);
+                    jogadores.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
+                    break;
+                case "Lateral":
+                    j = Lateral.parse(linhaPartida[1]);
+                    jogadores.put(j.getNumeroJogador(), j);
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
+                    break;
+                case "Avancado":
+                    j = Avancado.parse(linhaPartida[1]);
+                    jogadores.put(j.getNumeroJogador(), j); //.put coloca na
+                    if (ultima == null) throw new LinhaIncorretaException(); //we need to insert the player into the team
+                    ultima.insereJogador(j.clone()); //if no team was parsed previously, file is not well-formed
+                    break;
+                case "Jogo":
+                    Jogo jo = Jogo.parse(linhaPartida[1]);
+                    jogos.add(jo);
+                    break;
+                default:
+                    throw new LinhaIncorretaException();
 
             }
         }
-    }
-    //3. Manage team
-    private static void associatePlayer() throws IOException {
-        System.out.println("Nome do jogador a transferir ?");
-        String nome = in.nextLine();
-        System.out.println("Equipa de destino ?");
-        String destino = in.nextLine();
 
-        String fullLine = getFullData(nome);
-        System.out.println(fullLine);
-
-        updateDatabase(fullLine,"");
-        updateDatabase("Equipa:"+destino, "Equipa:"+destino+"\n"+fullLine);
-    }
-    //4. Create team
-    private static void createTeam() {
-        System.out.println("Nome da equipa:");
-        String name = in.nextLine();
-        try {
-            String content = "\nEquipa:" + name;
-            Files.write(Paths.get("src/proj/Jogadores.txt"), content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        //debug
+        for (Equipa e: equipas.values()){
+            System.out.println(e.toString());
         }
-    }
+        for (Jogo jog: jogos){
+            System.out.println(jog.toString());
 
-
-
-    //5. Create Player
-    private static void createPlayer() throws IOException {
-        //Recebe estatísticas
-        System.out.println("Nome do jogador:");
-        String name = in.nextLine();
-        System.out.println("Nº Jogador: ");int number = in.nextInt();
-        System.out.println("Velocidade: ");int velocidade = in.nextInt();
-        System.out.println("Resistência: ");int resistencia = in.nextInt();
-        System.out.println("Destreza: ");int destreza = in.nextInt();
-        System.out.println("Impulsao: ");int impulsao = in.nextInt();
-        System.out.println("Cabeça: ");int cabeca = in.nextInt();
-        System.out.println("Remate: ");int remate = in.nextInt();
-        System.out.println("Passe: ");int passe = in.nextInt();
-
-
-        //Recebe posição
-        System.out.println("Posição: \n1. Avançado\n2. Defesa\n3. Medio\n4. Lateral\n5. Guarda-Redes \n");
-        int posicao = in.nextInt();
-        int recuperacao = 0, cruzamento = 0, elasticidade = 0;
-        String posicaoString = null;
-
-            //Dependendo da posição escrever "Posição: Nome,vel,res,destr, etc..
-        switch (posicao) {
-            case 1:
-                posicaoString = "Avancado";
-                break;
-            case 2:
-                posicaoString = "Defesa";
-                break;
-            case 3:
-                posicaoString = "Medio";
-                System.out.println("Recuperação: ");
-                recuperacao = in.nextInt();
-                break;
-            case 4:
-                posicaoString = "Lateral";
-                System.out.println("Cruzamento: ");
-                cruzamento = in.nextInt();
-                break;
-            case 5:
-                posicaoString = "Guarda-Redes";
-                System.out.println("Elasticidade: ");
-                elasticidade = in.nextInt();
-                break;
-            default:
-                System.out.println("Posição inexistente");
-                break;
         }
 
-        //nao há perigo de posicaoString nao estar incializada, pois caso contrário não muda a database
-        if (posicao >= 1 && posicao <= 5) {
-            if (posicao == 1 || posicao == 2) { updateDatabase("Equipa:Temp", "Equipa:Temp\n"+posicaoString+":"+name+","+number+","+velocidade+","+resistencia+","+destreza+","+impulsao+","+cabeca+","+ remate +","+passe);}
-            else if (posicao == 3) {updateDatabase("Equipa:Temp", "Equipa:Temp\n"+posicaoString+":"+name+","+number+","+velocidade+","+resistencia+","+destreza+","+impulsao+","+cabeca+","+ remate +","+passe+","+recuperacao);}
-            else if (posicao == 4) {updateDatabase("Equipa:Temp", "Equipa:Temp\n"+posicaoString+":"+name+","+number+","+velocidade+","+resistencia+","+destreza+","+impulsao+","+cabeca+","+ remate +","+passe+","+cruzamento);}
-            else {updateDatabase("Equipa:Temp", "Equipa:Temp\n"+posicaoString+":"+name+","+velocidade+","+number+","+resistencia+","+destreza+","+impulsao+","+cabeca+","+ remate +","+passe+","+elasticidade);}
-        }
     }
 
-    //Se não existir equipa não realiza alterações mas não retorna erro
-    public static void updateDatabase(String regex, String replacement) throws IOException {
-        StringBuffer buffer = new StringBuffer();
-        Scanner fileinput = new Scanner(new File("src/proj/Jogadores.txt"));
-        //Ler linhas e adicionar ao StringBuffer
-        while (fileinput.hasNextLine()){
-            buffer.append(fileinput.nextLine() + System.lineSeparator());
-        }
-        String fileContent = buffer.toString();
-        fileinput.close();
-
-        fileContent = fileContent.replaceAll(regex,replacement);
-
-        FileWriter writer = new FileWriter("src/proj/Jogadores.txt");
-        writer.append(fileContent);
-        writer.flush();
+    public static List<String> lerFicheiro(String nomeFich) {
+        List<String> lines;
+        Path mypath = Paths.get(nomeFich);
+        try { lines = Files.readAllLines(mypath, StandardCharsets.UTF_8); }
+        catch(IOException exc) { lines = new ArrayList<>(); }
+        return lines;
     }
 
-    //obter linha inteira onde ocorre o nome do jogador
-    public static String getFullData(String palavra) throws IOException, NullPointerException {
-        Scanner fileInput = new Scanner(new File("src/proj/Jogadores.txt"));
-        while (fileInput.hasNextLine()){
-            String line = fileInput.nextLine();
-            if (line.contains(palavra)){ return line; }
-        }
-        return "nada";
-    }
-
-    public static void save(Jogo j){
-        System.out.println("Pretende gravar o jogo ? (1 se sim)");
-        int grav = in.nextInt();
-
-        if (grav == 1){
-            try {
-                String content = j.toString()+"\n";
-                Files.write(Paths.get("src/proj/Jogadores.txt"), content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-            } catch (IOException ex) {
-                ex.printStackTrace();
+    //LOAD SAVEEEEEEE
+    public static void loadSave()throws LinhaIncorretaException{
+        List<String> linhas = lerFicheiro("src/proj/Jogadores.txt");
+        Map<String, Equipa> equipas = new HashMap<>();
+        Map<Integer, Jogador> jogadores = new HashMap<>();
+        List<Jogo> jogos = new ArrayList<>();
+        Equipa ultima = null; Jogador j = null;
+        String[] linhaPartida;
+        for (String linha : linhas) {
+            linhaPartida = linha.split(":", 2);
+            switch(linhaPartida[0]){
+                case "Jogo":
+                    Jogo jo = Jogo.parse(linhaPartida[1]);
+                    jogos.add(jo);
+                    break;
+                default:
+                    break;
             }
         }
+        //Print saved games
+        for (Jogo jog: jogos){
+            System.out.println(jog.toString());
+        }
+
+
     }
+
 
 }
